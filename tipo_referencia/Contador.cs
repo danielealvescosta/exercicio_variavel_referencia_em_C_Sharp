344bdd20c64134f9ab45a5d43000d56158553c55
Alves <Alves@LAPTOP-Q663NCV4>
2023-04-03 08:16:22 -0300

    Adicionar arquivos de projeto.

---
+++ tipo_referencia/Contador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tipo_referencia
+{
+    internal class Contador
+    {
+        private int valor; //atributo
+        public Contador() //construtor para inicializar os atributos
+        {
+            valor = 5;
+        }
+
+        public void incrementa (int incremento)//chama o método incrementa - valor = 5+5=10
+        {
+            valor += incremento;
+        }
+
+        public void imprime()//chama o método imprime
+        {
+            Console.WriteLine("Valor: {0}", valor);
+        }
+    }
+}
---
+++ tipo_referencia/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tipo_referencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Contador obj1 = new Contador(); //cria um objeto tipo contador na memória
+
+            Contador obj2 = null; //variável de referência pra um objeto do tipo contador - atribui valor nulo
+
+            obj2 = obj1; //obj2 recebe o valor de referênia de obj1
+            //variaveis de referencia guardam localizações de um objeto
+            //obj1 e obj2 fazem referencia a um mesmo objeto
+
+            obj1.incrementa(5);
+            obj2.imprime();
+
+            obj2.incrementa(10); //chama o método incrementa - valor = 10+10=20
+            obj1.imprime();
+        }//total de instancias(objetos criados) do tipo contador = 1
+        //total de referencias para o objeto criado = 2 (obj1 e obj2)
+    }
+}
